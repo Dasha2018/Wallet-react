20f0038940a79e2b96fc8e6e9a98f6bf523722f8
Dasha2018 <zavadskayadasha97@gmail.com>
2025-06-08 17:54:51 +0700

    Вёрстка

---
+++ src/assets/GlobalStyles.js
@@ -0,0 +1,44 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+ * {
+    font-family: 'Montserrat', sans-serif;
+  }
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  *:before,
+  *:after {
+    box-sizing: border-box;
+  }
+
+  a,
+  a:visited {
+    text-decoration: none;
+    cursor: pointer;
+  }
+
+  button,
+  ._btn {
+    cursor: pointer;
+    outline: none;
+  }
+
+
+  body, html {
+    height: 100%;
+    font-family: "Montserrat", Arial, Helvetica, sans-serif;
+    background: #F4F5F6;
+  
+  }
+
+  body{
+    @media (max-width: 768px) {
+        background:#FFFFFF;
+    }
+  }
+
+`;
---
+++ src/components/Header/HLogo.jsx
@@ -0,0 +1,39 @@
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const HeaderLogoStyle = styled.div`
+  a {
+    display: block;
+  }
+`;
+
+const LogoImage = styled.img`
+  width: 144px;
+  height: 19px;
+
+  @media (max-width: 768px) {
+    width: 120px;
+    height: 16px;
+  }
+
+  @media (max-width: 480px) {
+    width: 109px;
+    height: 14px;
+  }
+`;
+
+function HeaderLogo({ logo }) {
+  return (
+    <HeaderLogoStyle>
+      <a href="/" target="_self">
+        <LogoImage src={logo} alt="logo" />
+      </a>
+    </HeaderLogoStyle>
+  );
+}
+
+HeaderLogo.propTypes = {
+  logo: PropTypes.string.isRequired,
+};
+
+export default HeaderLogo;
